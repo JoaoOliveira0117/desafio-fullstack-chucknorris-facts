e7246a8b1c3d0a7d441231ef4859c9da4bd4fa08
João Oliveira <joao.oliveira0117@hotmail.com>
2024-06-20 00:48:02 -0300

    remove boiler plate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      screens: {
+        xs: '565px',
+      },
+      colors: {
+        thistle: {
+          default: '#D3C0CD',
+        },
+        jet: {
+          light: '#4D4A5B',
+          default: '#3D3A4B',
+          dark: '#2D2A3B',
+        },
+        lavender: {
+          light: '#F5F5FF',
+          default: '#E3DFFF',
+          dark: '#CCCCFF',
+        },
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
